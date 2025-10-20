ca03a0a1032d56959a5d0d5aafaf226537f6d8a9
Mostafa <mostafa.magdy22987@gmail.com>
2025-10-20 03:10:48 +0300

    Add project files.

---
+++ WormReads/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WormReads.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
