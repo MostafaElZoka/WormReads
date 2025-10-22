eb952212be621d2c299b8a2bb9a9dc2b64eadacf
Mostafa <mostafa.magdy22987@gmail.com>
2025-10-22 05:54:20 +0300

    Implement generic repository pattern for data access

---
+++ WormReads.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace WormReads.DataAccess.Repository;
+
+public interface IRepository<T> where T : class
+{
+    public IEnumerable<T> GetAll();
+    public T Get(Expression<Func<T,bool>> filter);
+    public void Add(T entity);
+    public void Remove(T entity);
+    public void RemoveRange(IEnumerable<T> entities);
+}
---
+++ WormReads.DataAccess/Repository/Repository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WormReads.Data;
+
+namespace WormReads.DataAccess.Repository;
+
+internal class Repository<T>(AppDbContext dbContext) : IRepository<T> where T : class
+{
+    private DbSet<T> _dbset = dbContext.Set<T>();
+
+    public void Add(T entity)
+    {
+        _dbset.Add(entity);
+        dbContext.SaveChanges();
+    }
+
+    public T Get(Expression<Func<T, bool>> filter)
+    {
+        return _dbset.FirstOrDefault(filter);
+    }
+
+    public IEnumerable<T> GetAll()
+    {
+        return _dbset.ToList();
+    }
+
+    public void Remove(T entity)
+    {
+        _dbset.Remove(entity);
+        dbContext.SaveChanges();
+    }
+
+    public void RemoveRange(IEnumerable<T> entities)
+    {
+        _dbset.RemoveRange(entities);
+        dbContext.SaveChanges();
+    }
+}
