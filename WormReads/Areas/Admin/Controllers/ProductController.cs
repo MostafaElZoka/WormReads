161e8bf6f8d8c80769a0a660c0bcf76a3500fc23
Mostafa <mostafa.magdy22987@gmail.com>
2025-10-23 05:08:01 +0300

    Add product management features and update navigation
    - Updated navigation menu in `_Layout.cshtml` to rename "Category" to "Categories" and added a "Products" link.
    - Created `ProductController.cs` for managing products with methods for listing and creating products.
    - Added `Create.cshtml` for a product creation form with necessary fields and validation.
    - Introduced `Index.cshtml` to display a list of products with options to create, edit, or delete products, ordered by price.

---
+++ WormReads/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WormReads.DataAccess.Repository.Unit_Of_Work;
+using WormReads.Models;
+
+namespace WormReads.Areas.Admin.Controllers
+{
+    public class ProductController(IUnitOfWork unitOfWork) : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = unitOfWork._Product.GetAll();
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if(ModelState.IsValid)
+            {
+                unitOfWork._Product.Add(product);
+                unitOfWork.Save();
+                TempData["success"] = "Product created successfully";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+    }
+}
