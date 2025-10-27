ccf0126ef68d882781393da40a9806b17e1490ab
Mostafa <mostafa.magdy22987@gmail.com>
2025-10-27 05:55:16 +0300

    delete using sweetalert2

--- WormReads/wwwroot/js/product.js
+++ WormReads/wwwroot/js/product.js
@@ -18,14 +18,36 @@ function loadDataTable() {
                                         <a href="/admin/product/upsert/${data}" class="btn btn-primary mx-2">
                                             <i class="bi bi-pencil"></i> Edit
                                         </a>
-                                        <a href="/admin/product/delete/${data}" class="btn btn-secondary">
+                                        <a onClick=Delete("/admin/product/delete/${data}") class="btn btn-secondary">
                                             <i class="bi bi-trash3"></i> Delete
                                         </a>
-                                    </div>`
+                                    </div>` //passing the url in delete button as an event that calls the delete function in JS
                 }, "width": "10px",
                 orderable: false,
                 searchable: false
             }
         ]
     })
-}    ;
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                success: function (data) {
+                    $('#myTable').DataTable().ajax.reload();
+                    toastr.success(data.message);
+                }
+            })
+        }
+    });
+}                                            
