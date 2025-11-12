46b68c718668233faddb976055e5127a3a02312c
Mostafa <mostafa.magdy22987@gmail.com>
2025-11-12 04:06:19 +0200

    add update order info action

--- WormReads/Areas/Admin/Controllers/OrderController.cs
+++ WormReads/Areas/Admin/Controllers/OrderController.cs
@@ -7,20 +7,43 @@ namespace WormReads.Areas.Admin.Controllers
     [Area("Admin")]
     public class OrderController(IUnitOfWork unitOfWork) : Controller
     {
+        [BindProperty]
+        public OrderVM OrderVM { get; set; }
         public IActionResult Index()
         {
             return View();
         }
         public IActionResult Details(int id)
         {
-            OrderVM orderVM = new OrderVM()
+            OrderVM = new OrderVM()
             {
                 OrderHeader = unitOfWork._OrderHeader.Get(o => o.Id == id, includes: o => o.User),
                 OrderDetails = unitOfWork._OrderDetails.GetAll(o => o.OrderHeaderId == id, includes: o => o.Product)
             };
-            return View(orderVM);
+            return View(OrderVM);
         }
-
+        public IActionResult UpdateOrderDetails()
+        {
+            var orderHeaderFromDb = unitOfWork._OrderHeader.Get(o => o.Id == OrderVM.OrderHeader.Id);
+            orderHeaderFromDb.Name = OrderVM.OrderHeader.Name;
+            orderHeaderFromDb.City = OrderVM.OrderHeader.City;
+            orderHeaderFromDb.PhoneNumber = OrderVM.OrderHeader.PhoneNumber;
+            orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
+            orderHeaderFromDb.StreetAddress = OrderVM.OrderHeader.StreetAddress;
+            orderHeaderFromDb.State = OrderVM.OrderHeader.State;
+            if (OrderVM.OrderHeader.Carrier != null)
+            {
+                orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
+            }
+            if (OrderVM.OrderHeader.TrackingNumber != null)
+            {
+                orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+            }
+            unitOfWork._OrderHeader.Update(orderHeaderFromDb);
+            unitOfWork.Save();
+            return RedirectToAction(nameof(Details), new { id = OrderVM.OrderHeader.Id });
+        }
+        
 
         #region API
         public IActionResult GetAll(string? status)
